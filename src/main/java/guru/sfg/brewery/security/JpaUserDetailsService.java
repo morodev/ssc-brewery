8e80e45d098f432f68b07fcadcbee0e4aa9e45ec
Luca Moro <71976553+morodev@users.noreply.github.com>
2020-12-14 19:29:04 +0100

    update jpa user, implement UserDetails and Credentials Container in spring security

--- src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
+++ src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
@@ -34,16 +34,16 @@ public class JpaUserDetailsService implements UserDetailsService {
 
         log.debug("Getting User info via JPA");
 
-        User user = userRepository.findByUsername(username).orElseThrow(() -> {
+        return userRepository.findByUsername(username).orElseThrow(() -> {
             return new UsernameNotFoundException("User name: " + username + " not found");
         });
 
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+        /*new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
                 user.getEnabled(), user.getAccountNonExpired(), user.getCredentialsNonExpired(),
-                user.getAccountNonLocked(), convertToSpringAuthorities(user.getAuthorities()));
+                user.getAccountNonLocked(), convertToSpringAuthorities(user.getAuthorities()));*/
     }
 
-    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
+   /* private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
         if (authorities != null && authorities.size() > 0){
             return authorities.stream()
                     .map(Authority::getPermission)
@@ -52,5 +52,5 @@ public class JpaUserDetailsService implements UserDetailsService {
         } else {
             return new HashSet<>();
         }
-    }
+    }*/
 }
